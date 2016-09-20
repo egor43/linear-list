bfe9e4b2f7f41295b6ba6432b0ed728c24e10611
egor43 <egor43.rus@mail.ru>
2016-09-20 22:52:39 +0300

    Библиотека Линейный список дописана

--- Linear-List/ConsoleTest/Program.cs
+++ Linear-List/ConsoleTest/Program.cs
@@ -17,7 +17,13 @@ namespace ConsoleTest
             LL.AddFront(3);
             LL.AddFront(4);
             LL.AddFront(5);
-            LL.Add(33, 5);
+            LL.Add(33, 6);
+            LL.AddBack(0);
+            LL.Delete(7);
+            LinearList<int> FF = LL.SearchValueToLinearList(4);
+
+            //Console.WriteLine(LL.SearchValue(4));
+            //Console.WriteLine(LL.Print());
             Console.ReadLine();
         }
     }
--- Linear-List/Linear-List/LinearList.cs
+++ Linear-List/Linear-List/LinearList.cs
@@ -6,7 +6,8 @@ using System.Threading.Tasks;
 
 namespace Linear_List
 {
-    public class LinearList<T>
+    //TODO: Метод поиска по значению
+    public class LinearList<T> where T : struct
     {
         #region Свойства
         private Element Head { get; set; }
@@ -16,10 +17,10 @@ namespace Linear_List
         #region Вложенные классы
         internal class Element
         {
-            private T Value { get; set; }
+            internal T Value { get; set; }
             internal Element Next { get; set; }
 
-            public Element(T value, Element next)
+            internal Element(T value, Element next)
             {
                 Value = value;
                 Next = next;
@@ -42,6 +43,12 @@ namespace Linear_List
         #endregion
 
         #region Методы
+
+        public void Clear()
+        {
+            this.Head = null;
+            this.Length = 0;
+        }
         public void AddBack(T value)
         {
             if (Head == null)
@@ -100,7 +107,86 @@ namespace Linear_List
                 throw new Exception("Position not set correctly.");
             }
         }
-        #endregion
 
+        public string Print()
+        {
+            string result = "";
+            Element count = Head;
+            while (count != null)
+            {
+                result = result + Convert.ToString(count.Value) + " ";
+                count = count.Next;
+            }
+            return result;
+        }
+
+        public void Delete(int position)
+        {
+            if ((position > 0) && (position <= this.Length))
+            {
+                if (position == 1) this.Head = Head.Next;
+                else
+                {
+                    Element NewElement = Head;
+                    for (int i = 1; i < position; i++)
+                    {
+                        if (i + 1 != position) NewElement = NewElement.Next;
+                        else
+                        {
+                            if (position == this.Length) NewElement.Next = null;
+                            else
+                            {
+                                NewElement.Next = (NewElement.Next).Next;
+                            }
+                        }
+                    }
+                }
+                Length--;
+            }
+            else
+            {
+                throw new Exception("Position not set correctly.");
+            }
+        }
+
+        public int SearchValue(T ValueToSearch)
+        {
+            int Result = 0;
+            Element count = Head;
+            while (count != null)
+            {
+                Result++;
+                if (count.Value.ToString() == ValueToSearch.ToString()) return Result;
+                count = count.Next;
+            }
+            return -1;
+        }
+
+        public string SearchValueToString(T ValueToSearch)
+        {
+            int Result = 0;
+            Element count = Head;
+            while (count != null)
+            {
+                Result++;
+                if (count.Value.ToString() == ValueToSearch.ToString()) return String.Format("Element:{0} found at position:{1}", ValueToSearch, Result);
+                count = count.Next;
+            }
+            return String.Format("Element not found");
+        }
+
+        public LinearList<T> SearchValueToLinearList(T ValueToSearch)
+        {
+            int Result = 0;
+            Element count = Head;
+            while (count != null)
+            {
+                Result++;
+                if (count.Value.ToString() == ValueToSearch.ToString()) return new LinearList<T>(ValueToSearch);
+                count = count.Next;
+            }
+            return null;
+        }
+        #endregion
     }
 }
