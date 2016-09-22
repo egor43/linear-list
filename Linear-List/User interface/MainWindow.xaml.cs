b57b05ba354c2f9f3d1b85c14591d5abd3577945
egor43 <egor43.rus@mail.ru>
2016-09-22 18:05:48 +0300

    Начало работы над кодом пользовательского интерфейса

--- Linear-List/User interface/MainWindow.xaml.cs
+++ Linear-List/User interface/MainWindow.xaml.cs
@@ -12,17 +12,56 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using Linear_List;
 
 namespace User_interface
 {
-    /// <summary>
-    /// Логика взаимодействия для MainWindow.xaml
-    /// </summary>
     public partial class MainWindow : Window
     {
+        public Object MyList;
         public MainWindow()
         {
             InitializeComponent();
+            InterfaceOff();
+        }
+
+        private void btnClear_Click(object sender, RoutedEventArgs e)
+        {
+            tbInput.Text = "";
+            tbResult.Text = "";
+            InterfaceOff();
+            MyList = null;
+        }
+
+        private void RadioButton_Checked(object sender, RoutedEventArgs e)
+        {
+            InterfaceOn();
+            RadioButton rb = (RadioButton)sender;
+            if (rb.Tag.ToString() == "int") MyList = new LinearList<int>();
+            else MyList = new LinearList<double>();
+        }
+
+        public void InterfaceOff()
+        {
+            stpInput.IsEnabled = false;
+            wrpActionButtons.IsEnabled = false;
+            btnResult.IsEnabled = false;
+            rbInt.IsChecked = false;
+            rbDouble.IsChecked = false;
+            groupBox.IsEnabled = true;
+            tbResult.Text = "Для начала работы выберите тип списка (int/double)";
+            groupBox.Background = Brushes.LightGreen;
+        }
+
+        public void InterfaceOn()
+        {
+            stpInput.IsEnabled = true;
+            wrpActionButtons.IsEnabled = true;
+            btnResult.IsEnabled = true;
+            groupBox.IsEnabled = false;
+            groupBox.Background = Brushes.LightPink;
+            tbResult.Text = "Для изменения типа списка нажмите \"очистить\"";
         }
     }
+
 }
